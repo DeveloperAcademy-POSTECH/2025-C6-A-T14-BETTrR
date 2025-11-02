9c6e9c2c9b5405b0a8cc664a441751977e02edcd
Isa <dn0992@naver.com>
2025-11-02 20:41:28 +0900

    feat: Repository에 Word 관련 메서드 추가

--- Bettr/Bettr/Domain/Repositories/ScriptRepository.swift
+++ Bettr/Bettr/Domain/Repositories/ScriptRepository.swift
@@ -79,4 +79,21 @@ class ScriptRepository {
     func fetchFeedbackDetails(forFeedbackSummaryId feedbackSummaryId: Int64, in db: Database) throws -> [FeedbackDetail] {
         try FeedbackDetail.filter(Column("feedbackSummaryId") == feedbackSummaryId).fetchAll(db)
     }
+    
+    // MARK: - Word
+    func save(word: inout Word, in db: Database) throws -> Word {
+        try word.save(db)
+        return word
+    }
+
+    func fetchWords(forScriptId scriptId: Int64, in db: Database) throws -> [Word] {
+        try Word
+            .filter(Column("scriptId") == scriptId)
+            .order(Column("orderIndex"))
+            .fetchAll(db)
+    }
+
+    func deleteWords(forScriptId scriptId: Int64, in db: Database) throws {
+        try Word.filter(Column("scriptId") == scriptId).deleteAll(db)
+    }
 }
